7653ea665081a780059d0ca71761b1953e0fa104
Sujata Gunale <tidbitsjs@gmail.com>
2020-09-29 17:35:40 +0530

    mailbox heading

---
+++ src/container/Header/Head.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./head.css";
+
+class Head extends Component {
+  render() {
+    return (
+      <div className="mail-heading">
+        <div className="mail-icons">
+          <i className="fas fa-bars"></i>
+          <i className="fab fa-mailchimp"></i>
+          <h3>Gmail</h3>
+        </div>
+        <div className="mail-search">
+          <div className="search-input">
+            <i className="fas fa-search"></i>
+            <input type="text" placeholder="Search Email" />
+          </div>
+        </div>
+        <div className="mail-info">
+          <i className="far fa-question-circle"></i>
+          <i className="fas fa-cog"></i>
+          <i className="fab fa-app-store-ios"></i>
+          <i className="fas fa-user-circle"></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Head;
