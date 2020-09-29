6dcf037cc350289bb256ede8a41e4993198c174a
Sujata Gunale <tidbitsjs@gmail.com>
2020-09-29 18:26:23 +0530

    option compose

--- src/container/Body/Options/Option.js
+++ src/container/Body/Options/Option.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import "./option.css";
+import Compose from "./components/Compose";
 
 export class Option extends Component {
   render() {
     return (
       <div className="mail-options">
-        <div>Hello</div>
+        <div className="option-head">
+          <Compose />
+        </div>
+        <div className="option-bottom">
+          <i class="fas fa-user"></i>
+          <i class="fab fa-meetup"></i>
+        </div>
       </div>
     );
   }
---
+++ src/container/Body/Options/components/Compose.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Compose extends Component {
+  render() {
+    return (
+      <div className="head-compose">
+        <i class="fas fa-plus"></i>
+        <p>Compose</p>
+      </div>
+    );
+  }
+}
+
+export default Compose;
